8034961931a6c6835b49f2b5179b5799cb3c2456
najathi <najathi777@gmail.com>
2020-04-28 22:03:50 +0530

    redux update forms

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -6,17 +6,17 @@ import AddPerson from '../components/AddPerson/AddPerson';
 import * as actionTypes from '../store/actions';
 
 class Persons extends Component {
-    
-    render () {
+
+    render() {
         return (
             <div>
                 <AddPerson personAdded={this.props.onAddedPerson} />
                 {this.props.prs.map(person => (
-                    <Person 
+                    <Person
                         key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.props.onRemovedPerson(person.id)}/>
+                        name={person.name}
+                        age={person.age}
+                        clicked={() => this.props.onRemovedPerson(person.id)} />
                 ))}
             </div>
         );
@@ -31,8 +31,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAddedPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
-        onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
+        onAddedPerson: (name, age) => dispatch({ type: actionTypes.ADD_PERSON, personData: { name: name, age: age } }),
+        onRemovedPerson: (id) => dispatch({ type: actionTypes.REMOVE_PERSON, personId: id })
     }
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,25 +4,26 @@ const initialState = {
     persons: []
 };
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
         case actionTypes.ADD_PERSON:
             const newPerson = {
                 id: Math.random(), // not really unique but good enough here!
-                name: 'Max',
-                age: Math.floor( Math.random() * 40 )
+                name: action.personData.name,
+                age: action.personData.age
             }
             return {
                 ...state,
-                persons: state.persons.concat( newPerson )
+                persons: state.persons.concat(newPerson)
             }
         case actionTypes.REMOVE_PERSON:
             return {
                 ...state,
                 persons: state.persons.filter(person => person.id !== action.personId)
             }
+        default:
+            return state;
     }
-    return state;
 };
 
 export default reducer;
